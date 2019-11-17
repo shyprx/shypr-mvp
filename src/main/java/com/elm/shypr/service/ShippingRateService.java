2b99fa533da957c537ed0c0dda9907ad141da89f
naifs <naif.sami@hotmail.com>
2019-11-17 20:14:34 +0300

    SHY-003 jasper backend preperation

--- src/main/java/com/elm/shypr/service/ShippingRateService.java
+++ src/main/java/com/elm/shypr/service/ShippingRateService.java
@@ -10,8 +10,15 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.HashMap;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import java.io.InputStream;
+import java.text.ParseException;
+import net.sf.jasperreports.engine.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 
 @Service
 @Transactional
@@ -40,5 +47,36 @@ public class ShippingRateService {
         Set<ShippingRateDto> result = shippingRates.stream().map(e -> modelMapper.map(e, ShippingRateDto.class)).collect(Collectors.toSet());
 
         return result;
+    }
+
+
+    public ResponseEntity<?> generateBarcode(String awbNumber)
+            throws JRException, ParseException {
+        InputStream template = getClass().getResourceAsStream("/templates/reports/airWaybillbarcode.jrxml");
+        JasperReport report = JasperCompileManager.compileReport(template);
+
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("Barcode", "123-11111111");
+            map.put("MasterAWBBarcode", "123-11111111");
+            map.put("number", "pkg Numer");
+            map.put("ImagesDir", this.getClass().getResource("/templates/reports/images/").toString());
+            map.put("destnation", "dest");
+            map.put("AirportName", "airport");
+            map.put("Origin", "origin");
+            map.put("IssuedBy", "issuedby");
+            map.put("Packages", "noOfpkgs");
+            map.put("Total", "total");
+            map.put("Weight", "weight");
+            map.put("PkgWeight", 0.0);
+            map.put("Chargeable", 2.0);
+            map.put("Description", "desc");
+            map.put("Dimension", "dimen");
+            map.put("Note", "note");
+            map.put("From",  "from");
+            map.put("TO","to");
+        JasperPrint jasperPrint = JasperFillManager.fillReport(report, map, new JREmptyDataSource());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("content-disposition", "attachment; filename=" + "ShipmentBarcode.pdf");
+        return ResponseEntity.ok().headers(headers).body(JasperExportManager.exportReportToPdf(jasperPrint));
     }
 }
